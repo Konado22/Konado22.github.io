59db69c32d9841734c0859313a1e88c6224035ab
Konado22 <jessedraper2@gmail.com>
2024-10-07 14:35:04 -0600

    update pages settings

--- eportfolio/next.config.mjs
+++ eportfolio/next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     //utilized from https://github.com/gregrickaby/nextjs-github-pages Thanks Greg!
     output: "export",
-    basePath: "/nextjs-github-pages",
     images: {
         unoptimized: true,
       }
