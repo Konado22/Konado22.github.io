b20a73da55759150b004118cef0598264868e8c4
Konado22 <jessedraper2@gmail.com>
2024-10-08 13:24:36 -0600

    wip

--- eportfolio/next.config.mjs
+++ eportfolio/next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   //nexjs config files, NEXTJS IS AMAZING
     output: "export",
-    //basePath:'',
+    basePath:'/konado22.github.io',
+    assetPrefix: '/konado22.github.io',
     images: {
         unoptimized: true,
       }
