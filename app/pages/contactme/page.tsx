6dea5d87a203404842bfa6f450991a0911bc15ed
Konado22 <jessedraper2@gmail.com>
2024-10-17 16:42:18 -0600

    fix formatting on contact me

--- app/pages/contactme/page.tsx
+++ app/pages/contactme/page.tsx
@@ -8,8 +8,8 @@ export default function ContactMe() {
             <div className="space-y-10">
                 <h1 className="text-6xl font-bold">Contact Me</h1>
                 <br/><br/>
-                <h1 className="text-4xl">Thank you for taking the time to view my Portfolio and my accomplishments. I am proud of the progress I have made and look forward towards developing my skills and career.</h1>
-                <h1 className="text-4xl">For any inqueries regarding project opportunities or ways I can support your team today please reach out to me through any of the resources below.</h1>
+                <h1 className="text-2xl">Thank you for taking the time to view my Portfolio and my accomplishments. I am proud of the progress I have made and look forward towards developing my skills and career.</h1>
+                <h1 className="text-2xl">For any inqueries regarding project opportunities or ways I can support your team today please reach out to me through any of the resources below.</h1>
                 <div className="flex flex-col p-10">
                     <h1 className="text-3xl">Below I have also provided additional resouces such as my downloadable cover letter as well as updated resume</h1><br/><br/>
                     <Link href={''} target="_blank"><button className="bg-slate-300 border-2 rounded border-black hover:shadow-2xl text-2xl hover:font-bold border-spacing-3 p-5">Link to Cover Letter</button></Link><br/>
