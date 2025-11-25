935c0ff00774889937d621ae40db99a9af196aa6
Konado22 <jessedraper2@gmail.com>
2025-11-25 11:46:58 -0700

    fix yaml

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   //nexjs config files, NEXTJS IS AMAZING
     output: "export",
     
-  //  basePath:'/konado22.github.io',
-  //  assetPrefix: '/konado22.github.io',
+   basePath:'/konado22.github.io',
+   assetPrefix: '/konado22.github.io',
     images: {
         unoptimized: true,
       }
