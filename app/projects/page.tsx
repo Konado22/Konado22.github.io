f5520babda2cd569534d836defe553fa71a0e7bf
Konado22 <jessedraper2@gmail.com>
2025-12-09 09:10:18 -0700

    formatting

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,18 +13,31 @@ export default function Projects() {
           <div className="bg-slate-200">
             <h1 className="flex justify-center font-bold p-10">Pokedex</h1>
             <p>A NextJS application using API fetching and state</p>
+            <p >Link to repo</p>
           </div>
           <div className="bg-slate-200">
-            <h1 className="flex justify-center font-bold p-10">Animal Center Database</h1>
+            <h1 className="flex justify-center font-bold p-10">
+              Animal Center Database
+            </h1>
             <p>A Dash project using Python to help with animal adoption</p>
+            <p>Link to repo</p>
           </div>
           <div className="bg-slate-200">
-            <h1 className="flex justify-center font-bold p-10">Pirate Treasure game</h1>
-            <p>A Keras AI program that uses neural networks    to teach the program to solve a maze</p>
+            <h1 className="flex justify-center font-bold p-10">
+              Pirate Treasure game
+            </h1>
+            <p>
+              A Keras AI program that uses neural networks to teach the program
+              to solve a maze
+            </p>
+            <p>Link to repo</p>
           </div>
           <div className="bg-slate-200">
             <h1 className="flex justify-center font-bold p-10">Traveler</h1>
-            <p>A Fullstack NextJS application using Postgres to manage vacations</p>
+            <p>
+              A Fullstack NextJS application using Postgres to manage vacations
+            </p>
+            <p>Link to repo</p>
           </div>
         </div>
         <div className="flex-col leading-loose text-2xl">
