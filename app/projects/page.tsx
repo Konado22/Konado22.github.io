a5bbe17fa2c649388d8ac95de03f4265fa80bdda
Konado22 <jessedraper2@gmail.com>
2025-12-09 12:42:13 -0700

    add assets and cleanup projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Navbar from "../navbar/navbar";
+import Image from "next/image";
 
 export default function Projects() {
   return (
@@ -10,37 +11,98 @@ export default function Projects() {
           Projects
         </h1>
         <div className="p-10 grid grid-cols-2 text-2xl leading-loose">
-          <div className="p-10">
-            <h1 className="flex font-bold justify-center">Pokedex</h1>
-            <p className="flex justify-center">A NextJS application using API fetching and state</p>
-            <Link href='https://github.com/Konado22/THEDEX' className="flex justify-center text-blue-600">Link to repo</Link>
+          <div className="p-10 m-10 bg-slate-400 rounded-lg">
+            <h1 className="pb-10 flex font-bold justify-center text-white text-4xl">
+              Pokedex
+            </h1>
+            <div className="p-10 flex justify-center">
+              <Image
+                height={"400"}
+                width={"400"}
+                src={"/pokemon.png"}
+                alt="pokemon project"
+              ></Image>
+            </div>
+            <p className="flex justify-center text-white">
+              A NextJS application using API fetching and state
+            </p>
+            <Link
+              href="https://github.com/Konado22/THEDEX"
+              className="flex justify-center text-blue-600"
+            >
+              Link to repo
+            </Link>
           </div>
-          <div className="p-10">
-            <h1 className="flex font-bold justify-center">
+          <div className="p-10 m-10 bg-slate-400 rounded-lg">
+            <h1 className="pb-10 flex font-bold justify-center text-white text-4xl">
               Animal Center Database
             </h1>
-            <p className="flex justify-center">A Dash project using Python to help with animal adoption</p>
-            <Link href='https://github.com/Konado22/AnimalCenterDatabase' className="flex justify-center text-blue-600">Link to repo</Link>
+            <div className="p-10 flex justify-center">
+              <Image
+                height={"400"}
+                width={"400"}
+                src={"/animalCenter.png"}
+                alt="animal center database"
+              ></Image>
+            </div>
+            <p className="flex justify-center text-white">
+              A Dash project using Python to help with animal adoption
+            </p>
+            <Link
+              href="https://github.com/Konado22/AnimalCenterDatabase"
+              className="flex justify-center text-blue-600"
+            >
+              Link to repo
+            </Link>
           </div>
-          <div className="p-10">
-            <h1 className="flex font-bold justify-center">
+          <div className="p-10 m-10 bg-slate-400 rounded-lg">
+            <h1 className=" flex font-bold justify-center text-white text-4xl">
               Pirate Treasure game
             </h1>
-            <p className="flex justify-center">
+            <div className="flex justify-center p-10">
+              <Image
+                height="400"
+                width="400"
+                src="/result.png"
+                alt="results of script"
+              ></Image>
+            </div>
+            <p className="flex justify-center text-white">
               A Keras AI program that uses neural networks to teach the program
               to solve a maze
             </p>
-            <Link href='https://github.com/Konado22/PirateTreasureGame' className="flex justify-center text-blue-600">Link to repo</Link>
+            <Link
+              href="https://github.com/Konado22/PirateTreasureGame"
+              className="flex justify-center text-blue-600"
+            >
+              Link to repo
+            </Link>
           </div>
-          <div className="p-10">
-            <h1 className="flex font-bold justify-center">Traveler</h1>
-            <p className="flex justify-center">
-              A Fullstack NextJS application using Postgres to manage vacations
+          <div className="p-10 m-10 bg-slate-400 rounded-lg">
+            <h1 className="pb-10 flex font-bold justify-center text-white text-4xl">
+              Pad Protection
+            </h1>
+            <div className="flex justify-center p-10">
+              <Image
+                height="400"
+                width="400"
+                src="/padProtection.png"
+                alt="pad protection app"
+              ></Image>
+            </div>
+
+            <p className="flex justify-center text-white">
+              Asset management solution
             </p>
-            <Link href='' className="flex justify-center text-blue-600" >Link to repo</Link>
+            <Link
+              href="https://github.com/Konado22/Pad-Protection"
+              className="flex justify-center text-blue-600"
+            >
+              Link to repo
+            </Link>
           </div>
         </div>
-        <div className="text-2xl leading-loose">
+        <div className="m-10 p-10 text-2xl leading-loose">
           <h1 className="flex justify-center font-bold">
             Future projects in progress
           </h1>
